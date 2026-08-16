76cd5bc4b718018f0132b7c28f817d14cd76aa74
Ayush Mehta <ayushmehtabps@gmail.com>
2026-08-16 23:30:23 +0530

    setup db connection driver

--- backend/internal/database/database_connection.go
+++ backend/internal/database/database_connection.go
@@ -1,1 +1,40 @@
 package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectMongo()*mongo.Client{
+	mongoUri:=os.Getenv("MONGO_URI")
+
+	if mongoUri == "" {
+		log.Fatal("MONGODB_URI not set")
+	}
+
+	clientOptions:=options.Client().Apply(mongoUri).SetConnectionTimeOut(10*time.Second);
+
+	ctx,cancel:=context.WithTimeout(context.Background(),10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("Mongo connection failed:", err)
+	}
+
+	// Ping MongoDB to verify connection for network check and stability
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal("Mongo ping failed:", err)
+	}
+
+	log.Println("MongoDB connected successfully")
+
+
+	// return the client once everything is done
+	return client
+}
