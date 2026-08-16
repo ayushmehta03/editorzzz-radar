983af3eb69c187891111c78e9d79edaadfc684e2
Ayush Mehta <ayushmehtabps@gmail.com>
2026-08-16 23:59:05 +0530

    validator setup

--- backend/internal/controllers/auth_controller.go
+++ backend/internal/controllers/auth_controller.go
@@ -1,8 +1,40 @@
 package controllers
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"net/http"
+
+	"github.com/ayushmehta03/editorzzz-radar-backend/internal/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 
-func RegisterAccount(client *mongo.Client){
-	
+func RegisterAccount(client *mongo.Client)gin.HandlerFunc{
+	return func(c*gin.Context){
+
+
+		var hirers models.Hirers
+
+
+		if err:=c.ShouldBindJSON(&hirers);err!=nil{
+			 c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input data"})
+            return
+		}
+
+      validate := validator.New()
+        if err := validate.Struct(hirers); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{
+                "error":   "Validation failed",
+                "details": err.Error(),
+            })
+            return
+        }
+
+
+		
+
+
+	}
+
 }
